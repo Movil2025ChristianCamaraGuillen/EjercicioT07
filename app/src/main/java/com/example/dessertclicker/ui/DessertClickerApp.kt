6e2ed640bccc887b81c036fc10250ade783b7dc2
Christian Cámara <chcamaraguillen@gmail.com>
2025-12-07 18:36:31 +0100

    DessertClickerApp completo

--- app/src/main/java/com/example/dessertclicker/ui/DessertClickerApp.kt
+++ app/src/main/java/com/example/dessertclicker/ui/DessertClickerApp.kt
@@ -4,24 +4,36 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.dessertclicker.DessertClickerScreen
+import androidx.compose.ui.platform.LocalContext
+import com.example.dessertclicker.ui.components.DessertClickerAppBar
+import com.example.dessertclicker.ui.components.DessertClickerScreen
 
 @Composable
 fun DessertClickerApp(
     uiState: DessertUiState,
     onDessertClicked: () -> Unit
 ) {
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
-
+            DessertClickerAppBar(
+                onShareButtonClicked = {
+                    shareSoldDessertsInformation(
+                        context,
+                        uiState.dessertsSold,
+                        uiState.revenue
+                    )
+                }
+            )
         }
-    ) { contentPadding ->
+    ) { padding ->
         DessertClickerScreen(
             revenue = uiState.revenue,
             dessertsSold = uiState.dessertsSold,
             dessertImageId = uiState.currentDessertImageId,
             onDessertClicked = onDessertClicked,
-            modifier = Modifier.padding(contentPadding)
+            modifier = Modifier.padding(padding)
         )
     }
 }
