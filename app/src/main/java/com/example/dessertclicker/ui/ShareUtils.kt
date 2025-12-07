747445299cecbd9c6bad52d2e11a17bad3591160
Christian Cámara <chcamaraguillen@gmail.com>
2025-12-07 18:17:57 +0100

    creación de un archivo .kt para shareSoldDessertsInformation

---
+++ app/src/main/java/com/example/dessertclicker/ui/ShareUtils.kt
@@ -0,0 +1,31 @@
+package com.example.dessertclicker.ui
+
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.dessertclicker.R
+
+private fun shareSoldDessertsInformation(intentContext: Context, dessertsSold: Int, revenue: Int) {
+    val sendIntent = Intent().apply {
+        action = Intent.ACTION_SEND
+        putExtra(
+            Intent.EXTRA_TEXT,
+            intentContext.getString(R.string.share_text, dessertsSold, revenue)
+        )
+        type = "text/plain"
+    }
+
+    val shareIntent = Intent.createChooser(sendIntent, null)
+
+    try {
+        ContextCompat.startActivity(intentContext, shareIntent, null)
+    } catch (e: ActivityNotFoundException) {
+        Toast.makeText(
+            intentContext,
+            intentContext.getString(R.string.sharing_not_available),
+            Toast.LENGTH_LONG
+        ).show()
+    }
+}
